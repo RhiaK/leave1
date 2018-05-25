0664f5b688668ad19dce9f2b51c1f3a50ad57529
RhiaKe <rhiakaulitzke@gmail.com>
2018-05-25 17:31:40 -0600

    profile and destinations designed

---
+++ frontend/src/Destinations.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './Navigation';
+import {
+    Container,
+    Row,
+    Col,
+    Button
+} from 'reactstrap';
+
+
+class Destinations extends Component {
+  render() {
+    return (
+      <div className="App img2">
+        <Container className="nomargin">
+          <Row>
+            <Col>
+            <Navigation />
+            </Col>
+            <Col>
+            <p className="other">Other things will go here</p>
+            </Col>
+          </Row>  
+          <Row>
+            <Col>
+              <footer className="navfooter">
+              © 2018 BDABRK. All rights reserved.
+              </footer>
+            </Col>
+          </Row>
+        </Container>   
+      </div>
+    );
+  }
+}
+
+export default Destinations;
