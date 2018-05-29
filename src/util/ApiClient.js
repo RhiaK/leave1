b7e88808554d9ec5c619b66412d2445a59f6adb0
Bill Doody <wdoody@yahoo.com>
2018-05-29 10:10:48 -0600

    removing backend for deploy

--- src/util/ApiClient.js
+++ src/util/ApiClient.js
@@ -2,11 +2,24 @@ import axios from 'axios';
 import store from '../store';
 import { URL } from '../config/Api';
 
-export const apiClient = function() {
-        const token = store.getState().token;
-        const params = {
-            baseURL: URL,
-            headers: {'Authorization': 'Token ' + token}
-        };
-        return axios.create(params);
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+
+export const apiClient = function () {
+    const token = store.getState().token;
+    const params = {
+        baseURL: URL,
+        headers: { 'Authorization': 'Token ' + token }
+    };
+    return axios.create(params);
 }
+
+
+
+
+// import axios from 'axios';
+
+// export default axios.create({
+//   baseURL: `http://jsonplaceholder.typicode.com/`
+// });
